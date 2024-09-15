a7ff3aa591eda269357e4df07f12fed42a375778
abhikusuma1008 <107231413+abhikusuma1008@users.noreply.github.com>
2024-09-15 12:37:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".text",{
+    strings:["Frontend Developer","Python Developer","Web Developer","Cyber Security Analyst"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
